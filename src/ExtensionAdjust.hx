45210a0156c9b0a08a69309293100fc366a5878a
Null Divide <nulldivide@gmail.com>
2020-09-21 16:30:40 +0300

    Update ExtensionAdjust.hx

--- src/ExtensionAdjust.hx
+++ src/ExtensionAdjust.hx
@@ -7,15 +7,6 @@ import nme.JNI;
 
 class ExtensionAdjust {
 	
-	public static function test(inputValue:Int):Int
-	{
-#if android
-		return extension_adjust_test_jni(inputValue);
-#else
-		return extension_adjust_test(inputValue);
-#end
-	}
-
 	public static function trackEvent(eventName:String, eventData:Any = null):Void
 	{
 		var evtDataString:String = null;
@@ -39,14 +30,7 @@ class ExtensionAdjust {
 #end
 	}
 
-	private static function extension_adjust_test(val:Int):Int
-	{
-		trace('-----------------------> ExtensionAdjust: test default ok ${val}');
-		return val;
-	}
-
 #if android
-	private static var extension_adjust_test_jni = JNI.createStaticMethod ("org.haxe.extension.ExtensionAdjust", "test", "(I)I");
 	private static var extension_adjust_track_event_jni = JNI.createStaticMethod ("org.haxe.extension.ExtensionAdjust", "trackEvent", "(Ljava/lang/String;Ljava/lang/String;)V");
 #end
 }
